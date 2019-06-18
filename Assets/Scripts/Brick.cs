7e1239a0a9b1911099047a9e8ec4948919df5989
Matt Rathbun <mrathbun80@gmail.com>
2019-06-18 11:20:37 -0600

    Initial commit

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+	public AudioClip crack;
+	public Sprite[] hitSprites;
+	public static int breakableCount = 0;
+	
+	private int timesHit;
+	private LevelManager levelManager;
+	private bool isBreakable;
+
+	// Use this for initialization
+	void Start () {
+		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+		isBreakable = (this.tag == "Breakable");
+
+		if (isBreakable) {
+			breakableCount++;
+			Debug.Log(breakableCount);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		AudioSource.PlayClipAtPoint(crack, transform.position);
+	}
+	
+	void OnCollisionExit2D (Collision2D collision) {
+		if (isBreakable) {
+			timesHit++;
+			int maxHits = hitSprites.Length + 1;
+			
+			if (timesHit >= maxHits) {
+				breakableCount--;
+				Debug.Log(breakableCount);
+				levelManager.BrickDestroyed();
+				Destroy(gameObject);
+			} else {
+				LoadSprites();
+			}
+		}
+	}
+	
+	void LoadSprites () {
+		int spriteIndex = timesHit - 1;
+		
+		if (hitSprites[spriteIndex]) {
+			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		}
+	}
+	
+	// TODO Remove this method once we can actually win
+	void SimulateWin() {
+		levelManager.LoadNextLevel();
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+	public bool autoPlay = false;
+	
+	private Ball ball;
+	
+	void Start() {
+		ball = GameObject.FindObjectOfType<Ball>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!autoPlay) {
+			MoveWithMouse();
+		} else {
+			AutoPlay();
+		}
+	}
+	
+	void MoveWithMouse() {
+		float mousePositionInBlocks = Input.mousePosition.x / Screen.width * 16;
+		Vector3 paddlePosition = new Vector3 (Mathf.Clamp (mousePositionInBlocks, 0.5f, 15.5f), this.transform.position.y, 0f);
+		this.transform.position = paddlePosition;
+	}
+	
+	void AutoPlay() {
+		//		float mousePositionInBlocks = Input.mousePosition.x / Screen.width * 16;
+		Vector3 ballPostition = ball.transform.position;
+		Vector3 paddlePosition = new Vector3 (Mathf.Clamp (ballPostition.x, 0.5f, 15.5f), this.transform.position.y, 0f);
+		this.transform.position = paddlePosition;
+	}
+}
